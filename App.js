6f29275d66660ba93718035754cf9085e9186712
Mrgowda2004 <142484666+Mrgowda2004@users.noreply.github.com>
2023-08-30 20:17:01 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
 import Header   from'./components/Header';
 import Course from './components/Course';
 function App() {
   return (
     <>
       <Header/>
-      <Routes>
-        <Route path='course' element= {</Course>} />
-      </Routes>
+      <Course/>
+      
     </>
   )
 }
