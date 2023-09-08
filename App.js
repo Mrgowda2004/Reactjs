3c91f1c118fca46c0dba97bd8dde8477a32284ed
Mrgowda2004 <142484666+Mrgowda2004@users.noreply.github.com>
2023-09-08 11:44:00 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import Header   from'./components/Header';
-import Course from './components/Course';
+import './components/Coursecard';
+import './components/Header';
+import ImgMediaCard from './components/Coursecard';
+import Header from './components/Header';
+import Coursecard from './components/Coursecard';
+import Feedback from './components/Feedback';
+
 function App() {
   return (
-    <>
+    <div className="App">
       <Header/>
-      <Course/>
+      <Coursecard/>
+      <Feedback/>
       
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
