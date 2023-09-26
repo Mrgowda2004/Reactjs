a28861ec8f925b9e35e094d004d86fd152253f4c
Mrgowda2004 <142484666+Mrgowda2004@users.noreply.github.com>
2023-09-26 22:16:18 +0530

    Update Header.js

--- Header.js
+++ Header.js
@@ -1,146 +1,163 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { Paper, AppBar,Toolbar,Typography} from '@mui/material';
 import './Header.css';
-import { useNavigate } from 'react-router-dom';
-import { Paper } from '@mui/material';
-class Header extends Component {
-  constructor(props) {
-    super(props);
+import PersonPinIcon from '@mui/icons-material/PersonPin';
+import axios from 'axios'; 
 
-    this.state = {
-      fields: '',
-      academic: '',
-      career: '',
-      fieldsOfStudy: '',
-      skill: '',
-      specificInterest: '',
-    };
-  }
+const Header = () => {
+  const [formData, setFormData] = useState({
+    fields: '',
+    academic: '',
+    career: '',
+    fieldsOfStudy: '',
+    skill: '',
+    specificInterest: '',
+  });
 
-  handleInputChange = (event) => {
+  const handleInputChange = (event) => {
     const { name, value } = event.target;
-    this.setState({ [name]: value });
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   };
 
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(this.state);
+
+    // Send the form data to the Node.js server
+    axios.post('/api/profile', formData)
+      .then(response => {
+        console.log('Data saved successfully:', response.data);
+        // You can also redirect the user to another page or perform other actions here
+      })
+      .catch(error => {
+        console.error('Error while saving data:', error);
+      });
   };
 
-  render() {
-    return (
-      <Paper sx={{padding: '32px'}}>
-      <div className="container">
-        <h1>User Profile</h1>
-        <form id="profileForm" onSubmit={this.handleSubmit}>
-          <label htmlFor="fields">Areas of Interest:</label>
-          <select
-            id="fields"
-            name="fields"
-            value={this.state.fields}
-            onChange={this.handleInputChange}
-          >
-            <option value="">Select your Area of Interest</option>
-            <option value="Area">Programer</option>
-            <option value="Photography">Photography</option>
-            <option value="arts">Arts</option>
-            <option value="engineering">engineering</option>
-            <option value="Chemistry">Chemistry</option>
-            <option value="Management">Management</option>
-            <option value="Time Manegement">Time Manegement</option>
-             <option value="Biology">Biology</option>
-            <option value="Sociology">Sociology</option>
-            <option value="Gardering">Gardering</option>
-            {/* Add more options here */}
-          </select>
+  return (
+    <header >
+      <AppBar position='relative'>
+        <Toolbar>
+          <PersonPinIcon/>
+          <Typography variant='h6'>User Profile</Typography>
+        </Toolbar>
+      </AppBar>
+        <Paper sx={{ padding: '50px' }}>
+          <div className='container'>
+            <h1>User Profile</h1>
+            <form id='profileForm' onSubmit={handleSubmit}>
+              <label htmlFor='fields'>Areas of Interest:</label>
+              <select
+                id='fields'
+                name='fields'
+                value={formData.fields}
+                label="Age"
+                onChange={handleInputChange}
+              >
+                <option value=''>Select your Area of Interest</option>
+                <option value='Area'>Programmer</option>
+                <option value='Photography'>Photography</option>
+                <option value='arts'>Arts</option>
+                <option value='engineering'>Engineering</option>
+                <option value='Chemistry'>Chemistry</option>
+                <option value='Management'>Management</option>
+                <option value='Time Manegement'>Time Management</option>
+                <option value='Biology'>Biology</option>
+                <option value='Sociology'>Sociology</option>
+                <option value='Gardening'>Gardening</option>
+                {/* Add more options here */}
+              </select>
 
-          <label htmlFor="academic">Academic Background:</label>
-          <select
-            id="academic"
-            name="academic"
-            value={this.state.academic}
-            onChange={this.handleInputChange}
-          >
-            <option value="">Select your Academic Background</option>
-            <option value="SSLC">SSLC</option>
-            <option value="PUC">PUC</option>
-            <option value="Diploma">Diploma</option>
-            <option value="BA">BA</option>
-            <option value="Bcom">Bcom</option>
-            <option value="BSc">BSc</option>
-            <option value="Bsc(cs)">Bsc(cs)</option>
-            <option value="BCA">BCA</option>
-            <option value="MA">MA</option>
-            <option value="MCOM">Mcom</option>
-            <option value="MSC">Msc</option>
-            <option value="aMBA">MBA</option>
-            <option value="MCA">MCA</option>
-            <option value="NURSING">Nursing</option>
-            <option value="aBSCA">Bsc(Agri)</option>
-            <option value="MEDICAL">Medical</option>
-            {/* Add more options here */}
-          </select>
+              <label htmlFor='academic'>Academic Background:</label>
+              <select
+                id='academic'
+                name='academic'
+                value={formData.academic}
+                onChange={handleInputChange}
+              >
+                <option value=''>Select your Academic Background</option>
+                <option value='SSLC'>SSLC</option>
+                <option value='PUC'>PUC</option>
+                <option value='Diploma'>Diploma</option>
+                <option value='BA'>BA</option>
+                <option value='Bcom'>Bcom</option>
+                <option value='BSc'>BSc</option>
+                <option value='Bsc(cs)'>Bsc(cs)</option>
+                <option value='BCA'>BCA</option>
+                <option value='MA'>MA</option>
+                <option value='MCOM'>Mcom</option>
+                <option value='MSC'>Msc</option>
+                <option value='aMBA'>MBA</option>
+                <option value='MCA'>MCA</option>
+                <option value='NURSING'>Nursing</option>
+                <option value='aBSCA'>Bsc(Agri)</option>
+                <option value='MEDICAL'>Medical</option>
+                {/* Add more options here */}
+              </select>
 
-          <label htmlFor="career">Career Goals:</label>
-          <select
-            id="career"
-            name="career"
-            value={this.state.career}
-            onChange={this.handleInputChange}
-          >
-            <option value="">Select your Career Goals</option>
-            <option value="lea">Leadership</option>
-            <option value="dev">Development</option>
-            <option value="edu">Education</option>
-            <option value="pro">Productivity</option>
-            <option value="doc">Doctor</option>
-            <option value="eff">Efficiency</option>
-            <option value="polr">Political</option>
-            <option value="cre">Research</option>
-            {/* Add more options here */}
-            </select>
+              <label htmlFor='career'>Career Goals:</label>
+              <select
+                id='career'
+                name='career'
+                value={formData.career}
+                onChange={handleInputChange}
+              >
+                <option value=''>Select your Career Goals</option>
+                <option value='Leadership'>Leadership</option>
+                <option value='Development'>Development</option>
+                <option value='Education'>Education</option>
+                <option value='Productivity'>Productivity</option>
+                <option value='Doctor'>Doctor</option>
+                <option value='Efficiency'>Efficiency</option>
+                <option value='Political'>Political</option>
+                <option value='Research'>Research</option>
+                {/* Add more options here */}
+              </select>
 
-          <label htmlFor="fieldsOfStudy">Fields of Study:</label>
-          <textarea
-            id="fieldsOfStudy"
-            name="fieldsOfStudy"
-            value={this.state.fieldsOfStudy}
-            onChange={this.handleInputChange}
-            rows="4"
-          ></textarea>
+              <label htmlFor='fieldsOfStudy'>Fields of Study:</label>
+              <textarea
+                id='fieldsOfStudy'
+                name='fieldsOfStudy'
+                value={formData.fieldsOfStudy}
+                onChange={handleInputChange}
+                rows='4'
+              ></textarea>
 
-          <label htmlFor="skill">Skill Levels:</label>
-          <select
-            id="skill"
-            name="skill"
-            value={this.state.skill}
-            onChange={this.handleInputChange}
-          >
-            <option value="">Select your Skill Level</option>
-            <option value="skil1">Novice</option>
-            <option value="skil2">Advanced Beginner</option>
-            <option value="skil3">Competence</option>
-            <option value="skil4">Proficient</option>
-            <option value="skil5">Expert</option>
-            {/* Add more options here */}
-            </select>
+              <label htmlFor='skill'>Skill Levels:</label>
+              <select
+                id='skill'
+                name='skill'
+                value={formData.skill}
+                onChange={handleInputChange}
+              >
+                <option value=''>Select your Skill Level</option>
+                <option value='Novice'>Novice</option>
+                <option value='Advanced Beginner'>Advanced Beginner</option>
+                <option value='Competence'>Competence</option>
+                <option value='Proficient'>Proficient</option>
+                <option value='Expert'>Expert</option>
+                {/* Add more options here */}
+              </select>
 
-          <label htmlFor="specificInterest">Specific Areas of Interest:</label>
-          <textarea
-            id="specificInterest"
-            name="specificInterest"
-            value={this.state.specificInterest}
-            onChange={this.handleInputChange}
-            rows="4"
-          ></textarea>
+              <label htmlFor='specificInterest'>Specific Areas of Interest:</label>
+              <textarea
+                id='specificInterest'
+                name='specificInterest'
+                value={formData.specificInterest}
+                onChange={handleInputChange}
+                rows='4'
+              ></textarea>
 
-          <div className="button">
-            <button type="submit" >SUBMIT</button>
+              <div className='button'>
+                <button >SUBMIT</button>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
-      </Paper>
-    );
-  }
-}
+        </Paper>
+    </header>
+  );
+};
 
 export default Header;
