141901cacfaa3205f4499aa5dda5aedaa86a38b1
Mrgowda2004 <142484666+Mrgowda2004@users.noreply.github.com>
2023-10-12 12:29:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
-// server.js
-
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(cors(
-  {
-    origin: "http://localhost:3000",
-  }
-));
-
-// MongoDB connection
+// MongoDB Connection
 mongoose.connect('mongodb+srv://mrgowdacr2018:Mrg%409066052004@cluster0.gqhsoos.mongodb.net/Data?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 
-// Create a MongoDB schema and model (you'll need to define a schema based on your data structure)
-const userSchema = new mongoose.Schema({
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Define a MongoDB Schema and Model (e.g., Profile)
+const profileSchema = new mongoose.Schema({
   fields: String,
   academic: String,
   career: String,
@@ -27,29 +30,33 @@ const userSchema = new mongoose.Schema({
   skill: String,
   specificInterest: String,
 });
+const Profile = mongoose.model('Profile', profileSchema);
+app.post('/register', async (req, res) => {
+  const { fields, academic, career, fieldsOfStudy, skill, specificInterest } = req.body;
 
-const User = mongoose.model('User', userSchema);
+  const newProfile = new Profile({
+    fields,
+    academic,
+    career,
+    fieldsOfStudy,
+    skill,
+    specificInterest,
+  });
 
-app.use(bodyParser.json());
-app.use(cors());
-
-// Define API endpoints
-app.get("/", (req, res) => {
-  const formData = req.body;
-  const newUser = new User(formData);
-
-  newUser.save()
-    .then(() => {
-      res.json({ message: 'Data saved successfully' });
-    })
-    .catch((error) => {
-      console.error('Error while saving data:', error);
+  newProfile.save((err, profile) => {
+    if (err) {
+      console.error('Error while saving data:', err);
       res.status(500).json({ error: 'Internal server error' });
-    });
+    } else {
+      console.log('Data saved successfully:', profile);
+      res.status(200).json({ message: 'Data saved successfully' });
+    }
+  });
 });
+
 
 // Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
+
