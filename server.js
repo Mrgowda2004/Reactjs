7a47d24cb970d1894e6174825dbf7242376116d8
Mrgowda2004 <142484666+Mrgowda2004@users.noreply.github.com>
2023-10-16 21:39:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -21,7 +20,7 @@ db.once('open', () => {
 app.use(bodyParser.json());
 app.use(cors());
 
-// Define a MongoDB Schema and Model (e.g., Profile)
+// Define a MongoDB Schema and Model 
 const profileSchema = new mongoose.Schema({
   fields: String,
   academic: String,
@@ -30,11 +29,11 @@ const profileSchema = new mongoose.Schema({
   skill: String,
   specificInterest: String,
 });
-const Profile = mongoose.model('Profile', profileSchema);
-app.post('/register', async (req, res) => {
+const Header = mongoose.model('Header', profileSchema);
+app.post('/Header', async (req, res) => {
   const { fields, academic, career, fieldsOfStudy, skill, specificInterest } = req.body;
 
-  const newProfile = new Profile({
+  const newProfile = new Header({
     fields,
     academic,
     career,
@@ -43,15 +42,42 @@ app.post('/register', async (req, res) => {
     specificInterest,
   });
 
-  newProfile.save((err, profile) => {
-    if (err) {
-      console.error('Error while saving data:', err);
-      res.status(500).json({ error: 'Internal server error' });
-    } else {
-      console.log('Data saved successfully:', profile);
-      res.status(200).json({ message: 'Data saved successfully' });
-    }
+  newProfile.save().then((res)=>{
+    console.log('Data saved successfully:', Header);
+  }).catch(err=>{
+    console.error('Error while saving data:', err);
+  })
+});
+
+
+const feedbackSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  phone: String,
+  course: String,
+  Degree: String,
+  feedback: String,
+  rating: String,
+});
+const FeedbackForm = mongoose.model('feedback', feedbackSchema);
+app.post('/feedback', async (req, res) => {
+  const { name,email,phone,course,Degree,feedback,rating } = req.body;
+
+  const newProfile = new FeedbackForm({
+    name,
+    email,
+    phone,
+    course,
+    Degree,
+    feedback,
+    rating,
   });
+
+  newProfile.save().then((res)=>{
+    console.log('Data saved successfully:', FeedbackForm);
+  }).catch(err=>{
+    console.error('Error while saving data:', err);
+  })
 });
 
 
@@ -59,4 +85,3 @@ app.post('/register', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
